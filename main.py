066a415e3e8a0ab6be99ca277f57082332b60f95
yaoys <1303703312@qq.com>
2022-06-17 11:34:46 +0800

    增加日期工具类

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import yaoys.tools.log.logutil as log
-import yaoys.tools.database.mysql as mysql
-log.mylog().get_logger()
+from yaoysTools.log.logutil import getLogger
+from yaoysTools.log.logutil import log_info
+from yaoysTools.log.logutil import MY_LOG_INFO
+
+logger = getLogger(log_name='testMain', save_log2_file=True, log_level=MY_LOG_INFO, log_path='./demo/log')
+log_info('test', my_logger=logger)
